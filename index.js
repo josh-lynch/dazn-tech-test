a6fe5e121ea65a56741dee9de46cba63e2a0b52d
jdLYNC <joshlynch@live.co.uk>
2018-09-22 13:15:21 +0100

    lambda successfully accessing db data

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const AWS = require('aws-sdk');
+
+const tableName = process.env.TABLE_NAME;
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-2' });
+
 module.exports.handler = (event, context, callback) => {
-  console.log('EVENT: ', event);
+  console.log(JSON.stringify(event, null, 2));
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: 2
+    }
+  };
+  
+  documentClient.get(params, (err, data) => {
+    if (err) callback(err, null);
+    else {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(data)
+      }
+      callback(null, response);
+    }
+  });
+
 }
--- readme.md
+++ readme.md
@@ -2,4 +2,15 @@
 
 ## api testing instructions
 
+### deploying
+
+To redeploy this api run the following cli scripts;
+
+```
+yarn deploy-dev-service
+  // deploys lambda function api and db
+yarn seed
+  // seeds dummy data in db
+```
+
 ## scalability strategy
